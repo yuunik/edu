8bae3de551beb39b21309cb30ae9ce6d16aef39e
yuunik <yuunik_zhou@163.com>
2024-05-20 16:03:16 +0800

    feat: 讲师管理模块 - 整合 swagger

--- service/service-edu/src/main/java/com/yuunik/eduservice/EduApplication.java
+++ service/service-edu/src/main/java/com/yuunik/eduservice/EduApplication.java
@@ -2,8 +2,11 @@ package com.yuunik.eduservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+// 扫描公共模块的配置
+@ComponentScan(basePackages = {"com.yuunik"})
 public class EduApplication {
     public static void main(String[] args) {
         SpringApplication.run(EduApplication.class, args);
--- service/service-edu/src/main/java/com/yuunik/eduservice/controller/EduTeacherController.java
+++ service/service-edu/src/main/java/com/yuunik/eduservice/controller/EduTeacherController.java
@@ -3,6 +3,9 @@ package com.yuunik.eduservice.controller;
 
 import com.yuunik.eduservice.entity.EduTeacher;
 import com.yuunik.eduservice.service.EduTeacherService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,6 +19,7 @@ import java.util.List;
  * @author yuunik
  * @since 2024-05-20
  */
+@Api(description = "讲师管理模块接口")
 @RestController
 @RequestMapping("/eduservice/edu-teacher")
 public class EduTeacherController {
@@ -23,6 +27,7 @@ public class EduTeacherController {
     private EduTeacherService eduTeacherService;
 
     // 查询所有的讲师列表
+    @ApiOperation("查询所有的讲师列表")
     @GetMapping("/getTeacherInfoList")
     public List<EduTeacher> getTeacherInfoList() {
         List<EduTeacher> teacherList = eduTeacherService.list(null);
@@ -30,8 +35,9 @@ public class EduTeacherController {
     }
 
     // 根据 id 删除讲师
+    @ApiOperation("根据 id 删除讲师")
     @DeleteMapping("/deleteTeacherById/{id}")
-    public boolean deleteTeacherById(@PathVariable String id) {
+    public boolean deleteTeacherById(@ApiParam(name = "id", value = "讲师 id", readOnly = true) @PathVariable String id) {
         boolean flag = eduTeacherService.removeById(id);
         return flag;
     }
