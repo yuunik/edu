a4afdf519f61bf38b7de6cca55b25a4dfe6e0e48
yuunik <yuunik_zhou@163.com>
2024-05-21 17:26:21 +0800

    feat: 讲师管理模块 - 配置日志输出等级

--- common/service-base/src/main/java/com/yuunik/baseserive/exception/YuunikException.java
+++ common/service-base/src/main/java/com/yuunik/baseserive/exception/YuunikException.java
@@ -4,8 +4,16 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+/**
+ * 自定义异常类
+ * @author yuunik
+ * @since 1.0
+ * @version 1.0
+ */
 @Data
+// 生成无参数的构造方法
 @NoArgsConstructor
+// 生成有参数的构造方法
 @AllArgsConstructor
 public class YuunikException extends RuntimeException{
     // 请求响应码
