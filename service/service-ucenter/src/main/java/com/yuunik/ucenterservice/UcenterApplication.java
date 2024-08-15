2add652dcb90b3421b798408d0b3c15a6a89a274
yuunik <chorria.zhou@gmail.com>
2024-08-15 16:22:43 +0800

    feat: 会员api模块注册nacos

--- service/service-ucenter/src/main/java/com/yuunik/ucenterservice/UcenterApplication.java
+++ service/service-ucenter/src/main/java/com/yuunik/ucenterservice/UcenterApplication.java
@@ -3,11 +3,13 @@ package com.yuunik.ucenterservice;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan("com.yuunik")
 @MapperScan("com.yuunik.ucenterservice.mapper")
+@EnableDiscoveryClient // 注册 nacos
 public class UcenterApplication {
     public static void main(String[] args) {
         SpringApplication.run(UcenterApplication.class, args);
